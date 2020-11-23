4c3b5e9a8c9196a16a52b02ea5f5ebfaab576120
Oscar Dealtry <odealtry@gmail.com>
2020-11-23 16:49:08 +0000

    validations generated

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :reviews
+  has_many :venues
+  validates :first_name, :last_name, :date_of_birth, :address, presence: true
 end
