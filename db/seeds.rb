39963c9e19b713b9b7e4c9a6e88e5f836542a057
Bethany Armstrong <bethanyarmstrong1@live.co.uk>
2020-11-24 13:04:20 +0000

    backend of venues show page and random seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+
+Venue.destroy_all
+User.destroy_all
+
+beth = User.create!(first_name: 'Bethany', last_name: 'Armstrong', date_of_birth: '19950213', address: 'London', email: 'beth@email.com', password: 'password')
+
+
+
+10.times do
+  venue = Venue.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: 'Pub',
+    user: beth
+  )
+  venue.save!
+end
