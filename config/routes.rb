0194690079c4d04a872cd052d029b2ac2604c8da
John Tweeddale <j.tweeddale@outlook.com>
2020-11-25 17:07:23 +0000

    misc

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/users/:id/shortlist', to: 'pages#shortlist', as: :shortlist
-  # get '/users/:id/bookings', to: 'bookings#user_bookings', as: :user_bookings
   resources :bookings, only: [:index]
   resources :venues, only: :show do
     resources :reviews, only: [:new, :create, :index]
