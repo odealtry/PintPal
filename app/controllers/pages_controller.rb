bd85721185242b1e5d1f6dda4970b5affa9fd788
John Tweeddale <j.tweeddale@outlook.com>
2020-11-26 12:38:01 +0000

    change logic for booking and shortlist pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,7 +14,9 @@ class PagesController < ApplicationController
 
   def shortlist    
     @user = User.find(params[:id])
-    @shortlist = Booking.all.where(user_id: current_user)
+    # @shortlist = Booking.all.where(user_id: current_user)
+    @user_bookings = Booking.all.where(user_id: current_user)
+    @shortlist = @user_bookings.select { |booking| booking.confirmed == false }
   end
 end
 
