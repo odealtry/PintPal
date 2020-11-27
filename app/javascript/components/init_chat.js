f834086ac188f39cc130fc832a355c4205220115
Juan Garcia <juagarca@gmail.com>
2020-11-27 12:21:30 +0000

    added to event listeners to all the buttons

--- app/javascript/components/init_chat.js
+++ app/javascript/components/init_chat.js
@@ -1,16 +1,27 @@
+
+cons chatHTML = (bookingId) => {
+  const HTML = "<div id="close"><%= link_to "x", shortlist_path %></div>"
+}
+
+
+
 const initChat = () => {
-  const button = document.getElementById('chat');
-  if (button) {
-    button.addEventListener('click', (event) => {
-      const chat = document.getElementById('chat-popup');
-      console.log(chat.style.display);
-      if (chat.style.display !== "block") {
-        chat.style.display = "block";
-      } else {
-        chat.style.display = "none";
-      }
-    });
-  }
+  const chatButtons = document.querySelectorAll('.chat-button');
+  chatButtons.forEach((button) => {
+    if (button) {
+      button.addEventListener('click', (event) => {
+        const bookingId = event.currentTarget.id;
+        // console.log(bookingId);
+        const chat = document.getElementById('chat-popup');
+        if (chat.style.display !== "block") {
+          chat.style.display = "block";
+        } else {
+          chat.style.display = "none";
+        }
+        chat.innerHtml = insertAdjacentHTML('beforeend', chatHTML(bookingId));
+      });
+    }
+  });
 }
 
 export { initChat };
