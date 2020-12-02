6aef66510e015db8370663f74f67c0a6e66b7aaa
Oscar Dealtry <odealtry@gmail.com>
2020-12-02 15:02:11 +0000

    added user location button to search bar

--- app/javascript/components/map.js
+++ app/javascript/components/map.js
@@ -1,8 +1,5 @@
 const dynamicMapHeight = () => {
   const map = document.querySelector("#map");
-  // map.addEventListener("click", (event) => {
-  //   console.log("working");
-  // })
   map.style.height = `${window.innerHeight}px`;
 }
 
