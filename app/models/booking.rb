2fd0f9e900aae09793f52fff43445691054b9394
John Tweeddale <j.tweeddale@outlook.com>
2020-12-03 11:31:25 +0000

    add accepted rejected pending icons to bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,10 +1,10 @@
 class Booking < ApplicationRecord
-  before_save :default_values
+  # before_save :default_values
   belongs_to :user
   belongs_to :venue
   has_one :chatroom, dependent: :destroy
   validates :start, :party_size, presence: true
-  def default_values
-    self.confirmed = false if self.confirmed.nil?
-  end
+  # def default_values
+  #   self.confirmed = false if self.confirmed.nil?
+  # end
 end
