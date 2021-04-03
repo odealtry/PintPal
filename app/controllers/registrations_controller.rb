dce3fc13f364f18a012d2b7cc0f10401392d5b6c
Oscar Dealtry <odealtry@gmail.com>
2021-04-03 17:55:06 +0100

    trying to redirect to home after signup to avoid 500 error

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
 
   protected
+
   def after_update_path_for(resource)
     profile_path(resource)
   end
 
   def after_sign_up_path_for(resource)
-    root_path
+    pages_home_path(resource)
   end
 end
