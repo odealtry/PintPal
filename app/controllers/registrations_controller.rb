c24d6367c0373af51ce50a1199f72aeaf8365218
Oscar Dealtry <odealtry@gmail.com>
2021-04-03 18:39:20 +0100

    restored default profile pic and trying root redirection for user update as well

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_update_path_for(resource)
-    profile_path(resource)
+    root_path(resource)
   end
 
   def after_sign_up_path_for(resource)
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,10 @@ class User < ApplicationRecord
   validates :first_name, :last_name, presence: true
   has_many :messages
   has_one_attached :photo
-  # after_commit :set_default_avatar, on: :create
+  after_commit :set_default_avatar, on: :create
 
   private
-  # def set_default_avatar
-  #   self.photo.attach(io: File.open(Rails.root.join("app", "assets", "images", "avatar-1.png")), filename: 'default.png' , content_type: "image/png")
-  # end
+  def set_default_avatar
+    self.photo.attach(io: File.open(Rails.root.join("app", "assets", "images", "avatar-1.png")), filename: 'default.png' , content_type: "image/png")
+  end
 end
